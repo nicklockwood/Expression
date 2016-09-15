5f525c991ebcb8281e1f5fc7b4c8de3885527d7b
Nick Lockwood <nick@charcoaldesign.co.uk>
2016-09-15 13:53:43 +0100

    Initial Commit

---
+++ Expression/Expression.h
@@ -0,0 +1,19 @@
+//
+//  Expression.h
+//  Expression
+//
+//  Created by Nick Lockwood on 15/09/2016.
+//  Copyright © 2016 Nick Lockwood. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Expression.
+FOUNDATION_EXPORT double ExpressionVersionNumber;
+
+//! Project version string for Expression.
+FOUNDATION_EXPORT const unsigned char ExpressionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Expression/PublicHeader.h>
+
+
