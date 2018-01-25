f3019d76706fafd83d025ebdb66aef72dcd5c355
Nick Lockwood <nick@charcoaldesign.co.uk>
2018-01-25 18:12:28 +0000

    Made AnyExpression evaluation thread-safe

--- Sources/AnyExpression.swift
+++ Sources/AnyExpression.swift
@@ -150,78 +150,13 @@ public struct AnyExpression: CustomStringConvertible {
         impureSymbols: (Symbol) -> SymbolEvaluator?,
         pureSymbols: (Symbol) -> SymbolEvaluator?
     ) {
-        let mask = (-Double.nan).bitPattern
-        let indexOffset = 4
+        let box = NanBox()
 
-        func bitPattern(for index: Int) -> UInt64 {
-            assert(index > -indexOffset)
-            return UInt64(index + indexOffset) | mask
-        }
-
-        let nilBits = bitPattern(for: -1)
-        let falseBits = bitPattern(for: -2)
-        let trueBits = bitPattern(for: -3)
-
-        var values = [Any]()
-        func store(_ value: Any) -> Double {
-            switch value {
-            case let doubleValue as Double:
-                return doubleValue
-            case let boolValue as Bool:
-                return Double(bitPattern: boolValue ? trueBits : falseBits)
-            case let floatValue as Float:
-                return Double(floatValue)
-            case is Int, is UInt, is Int32, is UInt32:
-                return Double(truncating: value as! NSNumber)
-            case let uintValue as UInt64:
-                if uintValue <= 9007199254740992 as UInt64 {
-                    return Double(uintValue)
-                }
-            case let intValue as Int64:
-                if intValue <= 9007199254740992 as Int64, intValue >= -9223372036854775808 as Int64 {
-                    return Double(intValue)
-                }
-            case let numberValue as NSNumber:
-                // Hack to avoid losing type info for UIFont.Weight, etc
-                if "\(value)".contains("rawValue") {
-                    break
-                }
-                return Double(truncating: numberValue)
-            case _ where AnyExpression.isNil(value):
-                return Double(bitPattern: nilBits)
-            default:
-                break
-            }
-            values.append(value)
-            return Double(bitPattern: bitPattern(for: values.count - 1))
-        }
-        func loadIfStored(_ arg: Double) -> Any? {
-            let bits = arg.bitPattern
-            if bits & mask == mask {
-                switch bits {
-                case nilBits:
-                    return nil as Any? as Any
-                case trueBits:
-                    return true
-                case falseBits:
-                    return false
-                default:
-                    let index = Int(bits ^ mask) - indexOffset
-                    if values.indices.contains(index) {
-                        return values[index]
-                    }
-                }
-            }
-            return nil
-        }
-        func load(_ arg: Double) -> Any {
-            return loadIfStored(arg) ?? arg
-        }
         func loadNumber(_ arg: Double) -> Double? {
-            return loadIfStored(arg).map { ($0 as? NSNumber).map { Double(truncating: $0) } } ?? arg
+            return box.loadIfStored(arg).map { ($0 as? NSNumber).map { Double(truncating: $0) } } ?? arg
         }
         func equalArgs(_ lhs: Double, _ rhs: Double) throws -> Bool {
-            switch (AnyExpression.safeUnwrap(load(lhs)), AnyExpression.safeUnwrap(load(rhs))) {
+            switch (AnyExpression.safeUnwrap(box.load(lhs)), AnyExpression.safeUnwrap(box.load(rhs))) {
             case (nil, nil):
                 return true
             case (nil, _), (_, nil):
@@ -273,11 +208,11 @@ public struct AnyExpression: CustomStringConvertible {
                 switch symbol {
                 case .infix("+"):
                     return { args in
-                        switch (load(args[0]), load(args[1])) {
+                        switch (box.load(args[0]), box.load(args[1])) {
                         case let (lhs as String, rhs):
-                            return try store("\(lhs)\(AnyExpression.stringify(rhs))")
+                            return try box.store("\(lhs)\(AnyExpression.stringify(rhs))")
                         case let (lhs, rhs as String):
-                            return try store("\(AnyExpression.stringify(lhs))\(rhs)")
+                            return try box.store("\(AnyExpression.stringify(lhs))\(rhs)")
                         case let (lhs as Double, rhs as Double):
                             return lhs + rhs
                         case let (lhs as NSNumber, rhs as NSNumber):
@@ -296,8 +231,8 @@ public struct AnyExpression: CustomStringConvertible {
                         // TODO: find alternative approach that doesn't lose precision
                         try fn(args.map {
                             guard let doubleValue = loadNumber($0) else {
-                                _ = try AnyExpression.unwrap(load($0))
-                                try AnyExpression.throwTypeMismatch(symbol, args.map(load))
+                                _ = try AnyExpression.unwrap(box.load($0))
+                                try AnyExpression.throwTypeMismatch(symbol, args.map(box.load))
                             }
                             return doubleValue
                         })
@@ -306,13 +241,13 @@ public struct AnyExpression: CustomStringConvertible {
             } else if let fn = boolSymbols[symbol] {
                 switch symbol {
                 case .variable("false"):
-                    return { _ in Double(bitPattern: falseBits) }
+                    return { _ in NanBox.falseValue }
                 case .variable("true"):
-                    return { _ in Double(bitPattern: trueBits) }
+                    return { _ in NanBox.trueValue }
                 case .infix("=="):
-                    return { Double(bitPattern: try equalArgs($0[0], $0[1]) ? trueBits : falseBits) }
+                    return { try equalArgs($0[0], $0[1]) ? NanBox.trueValue : NanBox.falseValue }
                 case .infix("!="):
-                    return { Double(bitPattern: try equalArgs($0[0], $0[1]) ? falseBits : trueBits) }
+                    return { try equalArgs($0[0], $0[1]) ? NanBox.falseValue : NanBox.trueValue }
                 case .infix("?:"):
                     return { args in
                         guard args.count == 3 else {
@@ -321,15 +256,15 @@ public struct AnyExpression: CustomStringConvertible {
                         if let number = loadNumber(args[0]) {
                             return number != 0 ? args[1] : args[2]
                         }
-                        try AnyExpression.throwTypeMismatch(symbol, args.map(load))
+                        try AnyExpression.throwTypeMismatch(symbol, args.map(box.load))
                     }
                 default:
                     return { args in
                         // TODO: find alternative approach that doesn't lose precision
-                        try store(fn(args.map {
+                        try box.store(fn(args.map {
                             guard let doubleValue = loadNumber($0) else {
-                                _ = try AnyExpression.unwrap(load($0))
-                                try AnyExpression.throwTypeMismatch(symbol, args.map(load))
+                                _ = try AnyExpression.unwrap(box.load($0))
+                                try AnyExpression.throwTypeMismatch(symbol, args.map(box.load))
                             }
                             return doubleValue
                         }) != 0)
@@ -338,17 +273,17 @@ public struct AnyExpression: CustomStringConvertible {
             } else {
                 switch symbol {
                 case .variable("nil"):
-                    return { _ in Double(bitPattern: nilBits) }
+                    return { _ in NanBox.nilValue }
                 case .infix("??"):
                     return { args in
-                        let lhs = load(args[0])
+                        let lhs = box.load(args[0])
                         return AnyExpression.isNil(lhs) ? args[1] : args[0]
                     }
                 case let .variable(name):
                     guard name.count >= 2, "'\"".contains(name.first!) else {
                         return { _ in throw Error.undefinedSymbol(symbol) }
                     }
-                    let stringRef = store(String(name.dropFirst().dropLast()))
+                    let stringRef = box.store(String(name.dropFirst().dropLast()))
                     return { _ in stringRef }
                 default:
                     return { _ in throw Error.undefinedSymbol(symbol) }
@@ -361,11 +296,11 @@ public struct AnyExpression: CustomStringConvertible {
             expression,
             impureSymbols: { symbol in
                 if let fn = impureSymbols(symbol) {
-                    return { try store(fn($0.map(load))) }
+                    return { try box.store(fn($0.map(box.load))) }
                 }
                 if !shouldOptimize {
                     if let fn = pureSymbols(symbol) {
-                        return { try store(fn($0.map(load))) }
+                        return { try box.store(fn($0.map(box.load))) }
                     }
                     return defaultEvaluator(for: symbol)
                 }
@@ -376,13 +311,13 @@ public struct AnyExpression: CustomStringConvertible {
                     switch symbol {
                     case .variable, .function(_, arity: 0):
                         do {
-                            let value = try store(fn([]))
+                            let value = try box.store(fn([]))
                             return { _ in value }
                         } catch {
                             return { _ in throw error }
                         }
                     default:
-                        return { try store(fn($0.map(load))) }
+                        return { try box.store(fn($0.map(box.load))) }
                     }
                 }
                 return defaultEvaluator(for: symbol)
@@ -391,12 +326,17 @@ public struct AnyExpression: CustomStringConvertible {
 
         // These are constant values that won't change between evaluations
         // and won't be re-stored, so must not be cleared
-        let literals = values
+        let literals = box.values
 
+        // Evaluation isn't thread-safe due to shared values
         evaluator = {
-            defer { values = literals }
+            objc_sync_enter(box)
+            defer {
+                box.values = literals
+                objc_sync_exit(box)
+            }
             let value = try expression.evaluate()
-            return load(value)
+            return box.load(value)
         }
         self.expression = expression
     }
@@ -421,6 +361,85 @@ public struct AnyExpression: CustomStringConvertible {
 
 // Private API
 private extension AnyExpression {
+
+    // Value storage
+    final class NanBox {
+        private static let mask = (-Double.nan).bitPattern
+        private static let indexOffset = 4
+        private static let nilBits = bitPattern(for: -1)
+        private static let falseBits = bitPattern(for: -2)
+        private static let trueBits = bitPattern(for: -3)
+
+        private static func bitPattern(for index: Int) -> UInt64 {
+            assert(index > -indexOffset)
+            return UInt64(index + indexOffset) | mask
+        }
+
+        // Literal values
+        public static let nilValue = Double(bitPattern: nilBits)
+        public static let trueValue = Double(bitPattern: trueBits)
+        public static let falseValue = Double(bitPattern: falseBits)
+
+        // The values stored in the box
+        public var values = [Any]()
+
+        // Store a value in the box
+        public func store(_ value: Any) -> Double {
+            switch value {
+            case let doubleValue as Double:
+                return doubleValue
+            case let boolValue as Bool:
+                return boolValue ? NanBox.trueValue : NanBox.falseValue
+            case let floatValue as Float:
+                return Double(floatValue)
+            case is Int, is UInt, is Int32, is UInt32:
+                return Double(truncating: value as! NSNumber)
+            case let uintValue as UInt64:
+                if uintValue <= 9007199254740992 as UInt64 {
+                    return Double(uintValue)
+                }
+            case let intValue as Int64:
+                if intValue <= 9007199254740992 as Int64, intValue >= -9223372036854775808 as Int64 {
+                    return Double(intValue)
+                }
+            case let numberValue as NSNumber:
+                // Hack to avoid losing type info for UIFont.Weight, etc
+                if "\(value)".contains("rawValue") {
+                    break
+                }
+                return Double(truncating: numberValue)
+            case _ where isNil(value):
+                return NanBox.nilValue
+            default:
+                break
+            }
+            values.append(value)
+            return Double(bitPattern: NanBox.bitPattern(for: values.count - 1))
+        }
+
+        // Retrieve a value from the box, if it exists
+        func loadIfStored(_ arg: Double) -> Any? {
+            switch arg.bitPattern {
+            case NanBox.nilBits:
+                return nil as Any? as Any
+            case NanBox.trueBits:
+                return true
+            case NanBox.falseBits:
+                return false
+            case let bits:
+                guard var index = Int(exactly: bits ^ NanBox.mask) else {
+                    return nil
+                }
+                index -= NanBox.indexOffset
+                return values.indices.contains(index) ? values[index] : nil
+            }
+        }
+
+        // Retrieve a value if it exists, else return the argument
+        func load(_ arg: Double) -> Any {
+            return loadIfStored(arg) ?? arg
+        }
+    }
 
     // Throw a type mismatch error
     static func throwTypeMismatch(_ symbol: Symbol, _ args: [Any]) throws -> Never {
